14aef7972165fc354fdbe41c7949278c181ce2fa
‘Thymolzzw’ <1916268924@qq.com>
2021-11-28 08:16:00 +0800

    211128

---
+++ src/exhaustive/test.java
@@ -0,0 +1,10 @@
+package exhaustive;
+
+public class test {
+    public static void main(String[] args) {
+        int a = Integer.MAX_VALUE;
+        int b = 4;
+        System.out.println(a + " + " + b + " = " + (a+b));
+    }
+    
+}
